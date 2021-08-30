0b2bbdfcb04b89bca1c9450b6684d9321027c793
Ebin B <ebinb11>
2021-08-30 18:01:13 +0530

    Initial committ

---
+++ cloud-apigateway/src/test/java/com/cloudapigateway/cloudapigateway/CloudApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudapigateway.cloudapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
